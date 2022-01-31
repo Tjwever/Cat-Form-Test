601718ed4d4c4e7e2cbfd571717aa990ced74a5b
tjwever <timjwever@gmail.com>
2022-01-31 11:15:44 -0500

    Better way of doing things

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-//variables to reference the DOM
-let catForm = document.forms["cat-form"];
-let catType = document.querySelector(".cat-type");
-let shortGray = document.getElementById("short-gray");
-let calico = document.getElementById("calico");
-let snowWhite = document.getElementById("snow-white");
+//variables
+let shortGray = document.querySelector("#short-gray")
+let calico = document.querySelector("#calico");
+let snowWhite= document.querySelector("#snow-white");
+let catSelector = document.querySelector("#cat-type");
 
-hideAll();
-
-function getDropdown(sel) {
-  hideAll();
-  let theValue = document.getElementById(sel.options[sel.selectedIndex].value);
-  if(theValue == "default") {
-    hideAll();
-    theValue.style.display = "none";
-  }
-  theValue.style.display = "block";
-
+// Step 1: Hide the cats!
+function hideCats() {
+  shortGray.style.display="none"
+  calico.style.display="none"
+  snowWhite.style.display="none"
 }
 
-function hideAll() {
-  shortGray.style.display = "none";
-  calico.style.display = "none";
-  snowWhite.style.display = "none";
-
+// Step 2: Show a specific cat!
+function displayCat() {
+  // 2b: Click the drop down menu
+  catSelector.addEventListener("change", function display() {
+    // 2c: Select 'shortgray' from the drop-down
+    let catVal = catSelector.value;
+    // console.log(typeof catVal)
+    // 2a: Change the "display" of the cat variable to [something]
+    if (catVal === "short-gray") {
+      hideCats()
+      shortGray.style.display = "block"
+    } else if (catVal === "calico") {
+      hideCats()
+      calico.style.display = "block"
+    } else if (catVal === "snow-white") {
+      hideCats()
+      snowWhite.style.display = "block"
+    }
+   })
 }
+
+hideCats();
+displayCat()
+
